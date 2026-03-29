529af82ad4b6cfe3b04f9d5c4c0cbe88dc620eb9
0x1eef <0x1eef@hardenedbsd.org>
2026-03-28 21:12:30 -0300

    feat(functions): add ThreadGroup

---
+++ lib/llm/function/thread_group.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module LLM::Function
+  class ThreadGroup
+    ##
+    # @param [Array<Thread>] threads
+    #  An array of threads
+    # @return [LLM::Function::ThreadGroup]
+    def initialize(threads)
+      @threads = threads
+    end
+
+    ##
+    # Returns an array of function returns
+    # @return [Array<LLM::Function::Return>]
+    def wait
+      @threads.map(&:value)
+    end
+  end
+end
